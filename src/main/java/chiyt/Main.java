cac29189c5a9d7fb822b9bee71fcfa8b00069c36
Pat_Chi <Pat_Chi@pegatroncorp.com>
2025-06-04 16:27:14 +0800

    優化程式碼，省去RelationshipGraphImpl

--- src/main/java/chiyt/Main.java
+++ src/main/java/chiyt/Main.java
@@ -13,25 +13,25 @@ import chiyt.v2.RelationshipGraphAdapter;
 
 public class Main
 {
-	public static void main(String[] args) {
-		try {
-			String script = new String(Files.readAllBytes(Paths.get("script.txt")), StandardCharsets.UTF_8);
+    public static void main(String[] args) {
+        try {
+            String script = new String(Files.readAllBytes(Paths.get("script.txt")), StandardCharsets.UTF_8);
 
-			//v1
-			chiyt.v1.RelationshipAnalyzer superAnalyzer = new SuperRelationshipAdapter(new SuperRelationshipAnalyzer());
-			superAnalyzer.parse(script);
-			String name1 = "A";
-			String name2 = "K";
-			List<String> mutualFriends = superAnalyzer.getMutualFriends(name1, name2);
-			System.out.println("Mutual friends between " + name1 + " and " + name2 + ": " + mutualFriends);
+            //v1
+            chiyt.v1.RelationshipAnalyzer superAnalyzer = new SuperRelationshipAdapter(new SuperRelationshipAnalyzer());
+            superAnalyzer.parse(script);
+            String name1 = "A";
+            String name2 = "K";
+            List<String> mutualFriends = superAnalyzer.getMutualFriends(name1, name2);
+            System.out.println("Mutual friends between " + name1 + " and " + name2 + ": " + mutualFriends);
 
-			//v2
-			chiyt.v2.RelationshipAnalyzer graphAnalyzer = new RelationshipGraphAdapter();
-			RelationshipGraph friendGraph = graphAnalyzer.parse(script);
-			boolean result = friendGraph.hasConnection(name1, name2);
-			System.out.println("Are " + name1 + " and " + name2 + " connected? " + result);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            //v2
+            chiyt.v2.RelationshipAnalyzer graphAnalyzer = new RelationshipGraphAdapter();
+            RelationshipGraph friendGraph = graphAnalyzer.parse(script);
+            boolean result = friendGraph.hasConnection(name1, name2);
+            System.out.println("Are " + name1 + " and " + name2 + " connected? " + result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/chiyt/v2/RelationshipGraphAdapter.java
+++ src/main/java/chiyt/v2/RelationshipGraphAdapter.java
@@ -1,12 +1,22 @@
 package chiyt.v2;
 
+import java.util.Set;
+
+import org.jgrapht.Graph;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 
-public class RelationshipGraphAdapter implements RelationshipAnalyzer{
-	@Override
-	public RelationshipGraph parse(String script) {
-        SimpleGraph<String, DefaultEdge> friendGraph = new SimpleGraph<>(DefaultEdge.class);
+public class RelationshipGraphAdapter implements RelationshipAnalyzer, RelationshipGraph{
+    private Graph<String, DefaultEdge> friendGraph;
+
+    public RelationshipGraphAdapter() {
+        this.friendGraph = new SimpleGraph<>(DefaultEdge.class);
+    }
+
+    @Override
+    public RelationshipGraph parse(String script) {
+        this.friendGraph = new SimpleGraph<>(DefaultEdge.class);
         String[] lines = script.split("\n");
         for (String line : lines) {
             String[] parts = line.split(":");
@@ -21,6 +31,18 @@ public class RelationshipGraphAdapter implements RelationshipAnalyzer{
                 }
             }
         }
-		return new RelationshipGraphImpl(friendGraph);
+        return this;
+    }
+
+    @Override
+    public boolean hasConnection(String name1, String name2) {
+        if (!friendGraph.containsVertex(name1) || !friendGraph.containsVertex(name2)) {
+            return false;
+        }
+        ConnectivityInspector<String, DefaultEdge> inspector = new ConnectivityInspector<>(friendGraph);
+        // 取得 name1 所在的連通分量
+        Set<String> connectedSet = inspector.connectedSetOf(name1);
+        // 檢查 name2 是否在這個 set 裡
+        return connectedSet.contains(name2);
     }
 }
